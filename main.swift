66fbab4197c389b85776d8541db4a4f19d4b2f4c
Isaac Pandyan <isaac.pandyan@student.allenisd.org>
2022-10-07 21:17:47 -0500

    hm

--- main.swift
+++ main.swift
@@ -6,44 +6,37 @@ let unsortedIntegers = [5, 1, 4, 2, 8]
 // DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
 // ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
 
-//Helped A LOT by Reyn(e)/(a)rd, Avanish, Christian, Kevin, Isaac (myself), and Pranav
+//Helped A LOT by Reyn(e)/(a)rd, Avanish, Christian, Kevin, Isaac (myself), and Pranav, and Onik (unintentonally hahahha)
 // they xplained everything to me and I used my big braen to lern everything
 //Add your code below:
+import Foundation
 
-var pass = 0
-var swap = 0
-var all = 0
+var min = 0
+var arr : [String] = []
 
-var sortInt = unsortedIntegers
-
-func swap(sorted: inout [Int], index: Int, smallest: Int){
-    sorted.swapAt(index, smallest)
-    swap += 1
-    pass += 1
-    eqz += 1
+while let line = readLine() {
+    arr.append(line)
 }
 
-print("Pass: \(pass), Swaps: \(swap)/\(all), Array: \(sortInt)")
+for i in 0 ..< arr.count {
+    min = i
 
-var eqz = 0
-var Min = 0
-func selection(sortInt: inout [Int]){
-    for i in eqz..<sortInt.count{
-        Min = i
-        for int in eqz ..< sortInt.count{
-            if sortInt[Min] > sortInt[int]{
-                Min = int
-            }
-
+    for j in (i + 1) ..< arr.count {
+        if arr[j] < arr[min] {
+            min = j
         }
     }
-    swap(sorted: &sortInt, index:eqz, smallest: Min)
+    if (min != i) {
+        swapAt(array: &arr, index: i, minIndex: min)
+    }
 }
 
+for line in arr {
+    print(line)
+}
 
-while pass != sortInt.count - 1{
-    selection(sortInt: &sortInt)
-    all += swap
-    print("Pass: \(pass), Swaps: \(swap)/\(all), Array: \(sortInt)")
-    swap = 0
+func swapAt(array: inout [String], index: Int, minIndex: Int) {
+    let temp = array[index]
+    array[index] = array[minIndex]
+    array[minIndex] = temp
 }
